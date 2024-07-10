331361813904ed04ec77e0979ebfd95bbc7ee5cc
0vf <0vfx@proton.me>
2024-07-10 23:41:09 +0300

    utils/info: bump version to 0.5.0

--- utils/info.py
+++ utils/info.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-vinny_version = "0.4.1"
+vinny_version = "0.5.0"
 
 def get_vinny_version():
 	return vinny_version
