4916e78acb583404951231c7c27a775ae1b28919
0vf <0vfx@proton.me>
2024-07-24 07:46:20 +0300

    utils/info: bump version to 0.6.0-b1 first 0.6.0 beta till i sort every planned feature out

--- utils/info.py
+++ utils/info.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-vinny_version = "0.5.1"
+vinny_version = "0.6.0-b1"
 
 def get_vinny_version():
 	return vinny_version
